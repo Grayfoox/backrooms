dd9d4c25ea9ca12e34db112860d135d6642b9ee6
Dominik Krisztof <domsztof@gmail.com>
2020-04-16 19:02:53 +0200

    update project

--- Assets/scripts/Tile_generator.cs
+++ Assets/scripts/Tile_generator.cs
@@ -2,17 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Tile{
+    public  GameObject the_tile;
+    public float creation_time;
+
+    public Tile(GameObject t,float ct){
+    the_tile = t;
+    creation_time = ct;
+    }
+}
+
 public class Tile_generator : MonoBehaviour
 {
     public GameObject[] tile_prefabs;
     private float tile_size = 10.0f;
-    public Transform player_object_transform;
-    public int number_of_tile_on_screen;
+    public Transform player_last_transform;
+    public int number_of_tile_x;
+    public int number_of_tile_z;
+    
+    //hastable tiles = new Hastable();
 
     // Start is called before the first frame update
     void Start()
     {
+            for(int i = -number_of_tile_x; i < number_of_tile_x;i++){
+                //for(int j = -number_of_tile_z; i < number_of_tile_z;j++){
+                    int j = 0;
+                    int picked_tile = Random.Range(0, tile_prefabs.Length);
+                    
+                    Vector3 pos = new Vector3((i*player_last_transform.position.x),0.0f,(j*player_last_transform.position.z));
+                    
+                    GameObject t = (GameObject) Instantiate(tile_prefabs[0]);
+                    
+                    t.transform.position = pos;
 
+                    //string tile_name = "Tile_"+((int)(pos.x)).ToString() + "_" +((int)(pos.z)).ToString() +": "+picked_tile.ToString();
+                    //t.name = tile_name;
+                    
+                    //Tile tile = new Tile(t,updateTime);
+                    //tiles.Add(tile_name, tile);
+
+                //}
+            }
     }
 
     // Update is called once per frame
@@ -25,10 +56,4 @@ public class Tile_generator : MonoBehaviour
 
     }
 
-    private void Spawn_tile(int prefab_index = -1){
-        GameObject tile;
-        tile = Instantiate(tile_prefabs[0]) as GameObject; //todo random
-        //tile.transform.setParent
-        tile.transform.position = Vector3.forward;
-    }
 }
