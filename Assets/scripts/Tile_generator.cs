c56313413b1cf6b8c52bebe94f70530535b628cb
Dominik Krisztof <domsztof@gmail.com>
2020-04-16 23:54:18 +0200

    tile gens now generates infinetly

--- Assets/scripts/Tile_generator.cs
+++ Assets/scripts/Tile_generator.cs
@@ -17,8 +17,8 @@ public class Tile_generator : MonoBehaviour
     public GameObject[] tile_prefabs;
     private float tile_size = 10.0f;
     public Transform player_transform;
-    private Transform player_last_transform;
-
+    private float player_last_transform_x;
+    private float player_last_transform_z;
     public int number_of_tile_x;
     public int number_of_tile_z;
     
@@ -28,12 +28,13 @@ public class Tile_generator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player_last_transform = player_transform; 
+        player_last_transform_x = player_transform.position.x;
+        player_last_transform_z = player_transform.position.z;
             for(int i = -number_of_tile_x; i < number_of_tile_x;i++){
                 for(int j = -number_of_tile_z; j < number_of_tile_z;j++){
                     int picked_tile = Random.Range(0, tile_prefabs.Length);
                     
-                    Vector3 pos = new Vector3((i*tile_size+player_transform.position.x),0.0f,(j*tile_size+player_transform.position.z));
+                    Vector3 pos = new Vector3((i*tile_size+frnd(player_transform.position.x)),0.0f,(j*tile_size+frnd(player_transform.position.z)));
                     
                     GameObject t = (GameObject) Instantiate(tile_prefabs[picked_tile]);
                     t.transform.position = pos;
@@ -41,7 +42,7 @@ public class Tile_generator : MonoBehaviour
                     
                     string tile_name = "Tile_"+((int)(pos.x)).ToString() + "_" +((int)(pos.z)).ToString();
                     t.name = tile_name;
-                    tiles.Add(tile_name,"1");
+                    tiles.Add(tile_name,t);
 
                      t.transform.parent = parent_object.transform;
 
@@ -52,12 +53,13 @@ public class Tile_generator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       int x_move = (int)(Mathf.Floor((player_last_transform.position.x - player_transform.position.x)/tile_size)*tile_size);
-       int z_move = (int)(Mathf.Floor((player_last_transform.position.z - player_transform.position.z)/tile_size)*tile_size);
+        
+       int x_move = (int)(player_transform.position.x - player_last_transform_x);
+       int z_move = (int)(player_transform.position.z - player_last_transform_z);
        
-       if(Mathf.Abs(x_move)>tile_size || Mathf.Abs(z_move)>tile_size){
-            Debug.Log(x_move + " | " + z_move);
-            player_last_transform = player_transform; 
+       if(Mathf.Abs(x_move)>=tile_size || Mathf.Abs(z_move)>=tile_size){
+            player_last_transform_x = player_transform.position.x;
+            player_last_transform_z = player_transform.position.z;
             generate_tiles();
 
        }
@@ -70,19 +72,27 @@ public class Tile_generator : MonoBehaviour
             for(int i = -number_of_tile_x; i < number_of_tile_x;i++){
                 for(int j = -number_of_tile_z; j < number_of_tile_z;j++){
                     int picked_tile = Random.Range(0, tile_prefabs.Length);
-                    Vector3 pos = new Vector3((i*tile_size+player_transform.position.x),0.0f,(j*tile_size+player_transform.position.z)); //todo dat do ifu for more speed
+                    Vector3 pos = new Vector3(i*tile_size+frnd(player_transform.position.x),0.0f,j*tile_size+frnd(player_transform.position.z)); 
                     string tile_name = "Tile_"+((int)(pos.x)).ToString() + "_" +((int)(pos.z)).ToString();
 
-                    if(!tiles.ContainsKey(tile_name)){
+                    //TODO MAZAT MAPU
+                    //TODO chozeni sikmo dela blbosti kvuli zaokrouhlovani, je mozne vyjit z mapy, pri 10 a vic tiles to ale musi hrac ujit velky vzdalenosti
+
+                    if(!tiles.ContainsKey(tile_name)){      
                         GameObject t = (GameObject) Instantiate(tile_prefabs[picked_tile]);
                         t.transform.position = pos;
                         t.transform.Rotate(0.0f, (Random.Range(0, 5) * 90.0f), 0.0f,Space.Self);
-                
+                        tiles.Add(tile_name,t);
+                        
                         t.name = tile_name;
                         t.transform.parent = parent_object.transform;
                     }
                 }
             }
+    }
+
+    float frnd(float num){
+            return Mathf.Floor(num/10) * 10;
     }
 
 }
