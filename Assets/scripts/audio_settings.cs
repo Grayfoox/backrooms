8cbb13558cf2e42acaf8e808c4ddd1e6a7facf23
Adam Krása <adam.krasa@avast.com>
2020-04-17 21:44:06 +0200

    added music secret, fixed multiple sound issues

---
+++ Assets/scripts/audio_settings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio_settings : MonoBehaviour
+{
+    void Start()
+    {
+        AudioConfiguration configuration = AudioSettings.GetConfiguration();
+        configuration.dspBufferSize = 4096;
+    }
+}
---
+++ Assets/scripts/radio_pickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class radio_pickup : MonoBehaviour
+{
+    private AudioSource song;
+    private bool wasPlayed = false;
+    void Awake()
+    {
+        song = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player" && !wasPlayed)
+        {
+            song.Play();
+            wasPlayed = true;
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }    
+    }
+}
