940dcd94094b4e51f2f4e1013aa87d8b6b029fd8
Adam Krása <adam.krasa@avast.com>
2020-04-17 02:34:15 +0200

    textures fix + hum sound

---
+++ Assets/scripts/candle_behaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class candle_behaviour : MonoBehaviour
+{
+    private float now = 0f;
+    private float last = 0f;
+    private float beforelast = 0f;
+    void LateUpdate()
+    {
+        now = 20f + Random.Range(2, 5) + Mathf.PingPong(Time.time * 10, 10f);
+        GetComponent<Light>().range = (last + now + beforelast)/ 3;
+        last = now;
+        if (beforelast != last)
+        {
+            beforelast = last;
+        }
+    }
+}
